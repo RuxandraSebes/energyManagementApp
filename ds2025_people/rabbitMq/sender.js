493efdd78127206aef1718f25676ae2ecfe8abe1
RuxandraSebes <ruxandra.sebes@siteclick.ro>
2025-11-21 10:27:08 +0200

    added basic rabbitMQ

---
+++ ds2025_people/rabbitMq/sender.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    const queue = 'hello';
+
+    channel.assertQueue(queue, { durable: false });
+
+    setInterval(() => {
+      const msg = 'Hello world';
+      channel.sendToQueue(queue, Buffer.from(msg));
+      console.log(" [x] Sent %s", msg);
+    }, 1000); // 1 secunda
+  });
+});
